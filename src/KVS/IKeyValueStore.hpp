0167daaaa05c7f24238ec002e7dd319904574c67
Julian Jesacher <julianjesacher@gmail.com>
2023-03-13 16:02:08 +0100

    Adjust naming and interface for keyValueStore

--- src/KVS/IKeyValueStore.hpp
+++ src/KVS/IKeyValueStore.hpp
@@ -9,10 +9,15 @@
 class IKeyValueStore
 {
 public:
+    IKeyValueStore() = default;
+    virtual ~IKeyValueStore() = default;
+
     IKeyValueStore(const IKeyValueStore&) = delete;
     IKeyValueStore& operator=(const IKeyValueStore&) = delete;
 
-    virtual Status put(const std::string& key, const ByteArray& value, const WriteOptions& options) = 0;
-    virtual Status get(const std::string& key, ByteArray& value, const ReadOptions& options) = 0;
-    virtual Status erase(const std::string& key, const WriteOptions& options) = 0;
+    virtual Status put(const std::string& key, const ByteArray& value, const WriteOptions& options = WriteOptions{}) = 0;
+    virtual Status get(const std::string& key, ByteArray& value, const ReadOptions& options = ReadOptions{}) const = 0;
+    virtual Status erase(const std::string& key, const WriteOptions& options = WriteOptions{}) = 0;
+
+    virtual uint64_t get_size() const = 0;
 };
--- src/KVS/InMemoryKVS.cpp
+++ src/KVS/InMemoryKVS.cpp
@@ -1,5 +1,7 @@
 #include "InMemoryKVS.hpp"
 
+#include <cstring>
+
 // NOLINTNEXTLINE
 Status InMemoryKVS::put(const std::string& key, const ByteArray& value, const WriteOptions& options) {
     mapping_[key] = ByteArray(std::move(value));
@@ -7,13 +9,13 @@ Status InMemoryKVS::put(const std::string& key, const ByteArray& value, const Wr
 }
 
 // NOLINTNEXTLINE
-Status InMemoryKVS::get(const std::string& key, ByteArray& value, const ReadOptions& options) {
+Status InMemoryKVS::get(const std::string& key, ByteArray& value, const ReadOptions& options) const {
     if (!mapping_.contains(key)) {
         std::string error_msg{"The given key was not found"};
         return Status::new_not_found(ByteArray::new_allocated_byte_array(error_msg));
     }
 
-    value = mapping_[key];
+    value = mapping_.at(key);
     return Status::new_ok();
 }
 
