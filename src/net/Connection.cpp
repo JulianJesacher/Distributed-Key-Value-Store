0c5c1ffa425347f9adca9d076adbcc22eeafd07b
Julian Jesacher <julianjesacher@gmail.com>
2023-03-15 22:22:06 +0100

    Add features to connection class

--- src/net/Connection.cpp
+++ src/net/Connection.cpp
@@ -1,6 +1,22 @@
+#include <sys/socket.h>
+
 #include "Connection.hpp"
 
 namespace net {
+
+    ssize_t send(int fd, std::span<const char> data) {
+        return ::send(fd, data.data(), data.size_bytes(), 0);
+    }
+    ssize_t send(int fd, const char* data, uint64_t size) {
+        return send(fd, std::span<const char>(data, size));
+    };
+
+    ssize_t receive(int fd, std::span<char> buf) {
+        return ::recv(fd, buf.data(), buf.size_bytes(), 0);
+    }
+    ssize_t receive(int fd, char* buf, uint64_t size) {
+        return receive(fd, std::span<char>(buf, size));
+    }
 
     Connection::Connection(FileDescriptor&& fd, sockaddr_in client) {
         fd_ = std::move(fd);
@@ -9,5 +25,29 @@ namespace net {
 
     Connection::Connection(FileDescriptor&& fd) {
         fd_ = std::move(fd);
+    }
+
+    int Connection::fd() const {
+        return fd_.unwrap();
+    }
+
+    ssize_t Connection::send(const std::string& data) {
+        return net::send(fd_.unwrap(), data.c_str(), data.size());
+    }
+
+    ssize_t Connection::send(const char* data, uint64_t size) {
+        return net::send(fd_.unwrap(), data, size);
+    }
+
+    ssize_t Connection::receive(std::ostream& stream) const {
+        char buf[128];
+        std::span<char> data(buf, 128);
+
+        ssize_t bytes_received = net::receive(fd_.unwrap(), data);
+        auto it = data.begin();
+        for (ssize_t i = 0; i < bytes_received; i++) {
+            stream << (*it++);
+        }
+        return bytes_received;
     }
 }
--- src/net/Connection.hpp
+++ src/net/Connection.hpp
@@ -2,14 +2,31 @@
 
 #include <optional>
 #include <arpa/inet.h>
+#include <span>
+#include <string>
+#include <istream>
 
 #include "FileDescriptor.hpp"
 
 namespace net {
+
+    ssize_t send(int fd, std::span<const char> data);
+    ssize_t send(int fd, const char* data, uint64_t size);
+
+    ssize_t receive(int fd, std::span<char> buf);
+    ssize_t receive(int fd, char* buf, uint64_t size);
+
     class Connection {
     public:
         Connection(FileDescriptor&& fd, sockaddr_in client);
         explicit Connection(FileDescriptor&& fd);
+
+        int fd() const;
+
+        ssize_t send(const std::string& data);
+        ssize_t send(const char* data, uint64_t size);
+
+        ssize_t receive(std::ostream& stream) const;
 
     private:
         FileDescriptor fd_;
