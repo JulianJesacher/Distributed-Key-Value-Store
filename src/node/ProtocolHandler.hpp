56b03b22207ffe8d2aa30f7f2f127746789eda81
Julian Jesacher <julianjesacher@gmail.com>
2023-03-16 23:11:00 +0100

    Add types

--- src/node/ProtocolHandler.hpp
+++ src/node/ProtocolHandler.hpp
@@ -1,3 +1,5 @@
+#pragma once
+
 #include <cstdint>
 #include <vector>
 
@@ -9,6 +11,11 @@ namespace node {
 
     namespace protocol {
 
+        template<typename E>
+        constexpr auto to_integral(E e) noexcept {
+            return static_cast<std::underlying_type_t<E>>(e);
+        }
+
         // Package structure: metadata | command_1_size | command_1 | command_2_size | command_2 | ... | payload_size | payload
         // commandX_size is of type uint64_t
         enum class Instruction: uint16_t {
@@ -17,20 +24,39 @@ namespace node {
             c_REMOVE = 2,
         };
 
-        struct metadata {
+        struct MetaData {
             uint16_t argc;
             Instruction instruction;
             uint64_t command_size;
             uint64_t payload_size;
         };
 
+        enum class CommandFieldsPut {
+            c_KEY = 0,
+            c_CUR_PAYLOAD_SIZE = 1,
+            c_OFFSET = 2,
+            //No value since that is stored directly in the byte array and not as part of the commands
+        };
+
+        enum class CommandFieldsGet {
+            c_KEY = 0,
+        };
+
+        enum class CommandFieldsRemove {
+            c_KEY = 0,
+        };
+
         using command = std::vector<std::string>;
 
-        metadata get_metadata(net::Connection& connection);
+        MetaData get_metadata(net::Connection& connection);
 
         command get_command(net::Connection& connection, uint16_t argc, uint64_t command_size, bool payload_exists = false);
 
         ByteArray get_payload(net::Connection& connection, uint64_t payload_size);
+
+        void get_payload(net::Connection& connection, char* dest, uint64_t payload_size);
+
+        void get_payload(net::Connection& connection, char* dest, uint64_t payload_size);
     }
 }
 
