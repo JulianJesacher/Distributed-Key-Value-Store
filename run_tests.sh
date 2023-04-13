33388860966ed3b985534e71ced2241dd2f48bc4
Julian Jesacher <julianjesacher@gmail.com>
2023-04-13 15:46:51 +0200

    Print which test is being executed

--- run_tests.sh
+++ run_tests.sh
@@ -1,10 +1,24 @@
 cd build
 cmake ..
 make
+
+echo "Running byteArrayTest"
 ./tests/byteArrayTest
+
+echo "Running clusterTest"
 ./tests/clusterTest
+
+echo "Running commandHandlerTest"
 ./tests/instructionHandlerTest
+
+echo "Running keyValueStoreTest"
 ./tests/keyValueStoreTest
+
+echo "Running networkingTest"
 ./tests/networkingTest
+
+echo "Running protocolHandlerTest"
 ./tests/protocolHandlerTest
+
+echo "Running clientTest"
 ./tests/clientTest
